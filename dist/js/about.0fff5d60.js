38772bec5d393990b5b44dbfb39d10a2073ac538
Anton Angelgardt <a.n.angelgardt@gmail.com>
2024-11-09 14:03:48 +0300

    build & add dist subtree

--- dist/js/about.0fff5d60.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunknglgrdt"]=self["webpackChunknglgrdt"]||[]).push([[594],{603:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var r=u(6768);const a={class:"about"};function e(n,t){return(0,r.uX)(),(0,r.CE)("div",a,t[0]||(t[0]=[(0,r.Lk)("h1",null,"This is an about page",-1)]))}var s=u(1241);const c={},l=(0,s.A)(c,[["render",e]]);var i=l}}]);
-//# sourceMappingURL=about.0fff5d60.js.map
