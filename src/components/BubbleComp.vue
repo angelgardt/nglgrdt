27dfe23c326e37da0a84ff820c7f466e4323f60b
Anton Angelgardt <a.n.angelgardt@gmail.com>
2024-11-12 11:14:34 +0300

    move en ru to BasicSection2.json, restore prev BasicSection.json, add ItemComp & BubbleComp

---
+++ src/components/BubbleComp.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="bubble">
+        text
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BubbleComp'
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/variables.scss";
+
+.bubble {
+    background-color: $silver;
+}
+</style>
---
+++ src/components/ItemComp.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="item">
+        text
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ItemComp'
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/variables.scss";
+
+.item {
+    background-color: $silver;
+}
+</style>
